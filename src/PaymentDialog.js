e2764bcefe2a73911669fb954c1cafbe587538ec
kaulsalil88 <kaulsalil88@gmail.com>
2023-11-18 11:39:16 +0530

    Added the other text fields

--- src/PaymentDialog.js
+++ src/PaymentDialog.js
@@ -13,6 +13,12 @@ const PaymentDialog = ({ open, onClose }) => {
   const [description, setDescription] = useState("");
   const [submitting, setSubmitting] = useState(false);
 
+  const isSubmitDisabled = !!to && !!from && !amount && isNaN(amount);
+
+  const handleSubmit = async () => {
+    setSubmitting(true);
+  };
+
   return (
     <Dialog open={open} onClose={onClose}>
       <DialogTitle>Payment Details</DialogTitle>
@@ -27,7 +33,37 @@ const PaymentDialog = ({ open, onClose }) => {
             !!to && !/^\S+@\S+\.\S+$/.test(to) ? "Invalid email address" : ""
           }
         />
+        <TextField
+          label="From"
+          select
+          fullWidth
+          value={from}
+          onChange={(e) => setFrom(e.target.value)}
+        >
+          <MenuItem value="USD">USD</MenuItem>
+          <MenuItem value="INR">INR</MenuItem>
+        </TextField>
+        <TextField
+          label="Amount"
+          type="number"
+          fullWidth
+          value={amount}
+          onChange={(e) => setAmount(e.target.value)}
+          error={!!amount && isNaN(Number(amount))}
+          helperText={!!amount && isNaN(Number(amount)) ? "Invalid Amount" : ""}
+        />
+        <TextField
+          label="Description"
+          value={description}
+          onChange={(e) => setDescription(e.target.value)}
+        />
       </DialogContent>
+      <DialogActions>
+        <Button onClick={onClose} color="primary">
+          Cancel
+        </Button>
+        <Button></Button>
+      </DialogActions>
     </Dialog>
   );
 };
